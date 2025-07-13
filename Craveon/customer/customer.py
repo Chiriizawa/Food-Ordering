20f29bb18ba468d86ca1eeb5cbd45bdcc9cf8680
Chiriizawa <you@example.com>
2025-07-14 05:58:52 +0800

    message

--- Craveon/customer/customer.py
+++ Craveon/customer/customer.py
@@ -19,10 +19,10 @@ def make_header(response):
 
 DB_CONFIGS = {
     'local': {
-        'host': '192.168.1.4',
+        'host': '10.0.30.32',
         'database': 'craveon',
         'user': 'root',
-        'password': 'haharaymund',
+        'password': 'ClodAndrei8225',
     },
     'flask_connection': {
         'host': '192.168.1.65',
